5354b7522935549386d7792fa2e3ca7037fe2271
leungSino <160813495+leungSino@users.noreply.github.com>
2025-07-31 23:39:40 +0800

    Update config.ts

--- modal-login/config.ts
+++ modal-login/config.ts
@@ -28,7 +28,7 @@ export const config = createConfig(
     transport: alchemy({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY! }),
     chain: sepolia,
     ssr: true, // more about ssr: https://accountkit.alchemy.com/react/ssr
-    storage: cookieStorage, // more about persisting state with cookies: https://accountkit.alchemy.com/react/ssr#persisting-the-account-state
+    //storage: cookieStorage, // more about persisting state with cookies: https://accountkit.alchemy.com/react/ssr#persisting-the-account-state
     enablePopupOauth: true, // must be set to "true" if you plan on using popup rather than redirect in the social login flow
     sessionConfig: {
       // If using sever-side signing, ideally this duration is <= the MIN_KEY_DURATION_REMAINING_MS defined on the server.
